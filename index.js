0ee43bde2b463e8708c751c74a69bf19c2f2c41e
Mxolisi <nyawose57@gmail.com>
2022-07-29 20:34:23 +0200

    saving changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const exphbs  = require('express-handlebars');
 const bodyParser =require('body-parser');
 const SettingsBill = require('./settingsBill');
-// const moment= require('moment') 
+const moment = require('moment');
 
 const app = express();
 const settingsBill = SettingsBill();
@@ -46,6 +46,7 @@ app.get('/actions', function(req,res){
     let bills = settingsBill.actions()
 
     for(let key of bills){
+        key.timestamp = moment(key.timestamp,'MMMM Do YYYY, h:mm:ss' ).fromNow()
     }
     res.render("actions",{
         actions: bills
